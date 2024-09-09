669e271c3c9b9407f5c5faa1faf51476be135358
Dipesh chaudhary <dipeshch040313@gmail.com>
2024-09-09 21:20:59 +0530

    navbar

--- frontend/src/page/StudentServices.tsx
+++ frontend/src/page/StudentServices.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar } from "../components/Navbar";
+import { NavbarDemo } from "../components/Navbar";
 import { WhyUsSection } from "../components/WhyUsSection";
 import { ProjectShowcaseSection } from "../components/ProjectShowcaseSection";
 import { TestimonialsSection } from "../components/TestimonialsSection";
@@ -7,7 +7,7 @@ import { TestimonialsSection } from "../components/TestimonialsSection";
 const StudentServices: React.FC = () => {
   return (
     <div>
-      <Navbar />
+      <NavbarDemo />
 
       {/* Main content */}
       <main className="pt-16">
