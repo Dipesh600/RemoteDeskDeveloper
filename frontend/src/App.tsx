c736faedb8760c0e3de43625498a40be90c66a79
Dipesh chaudhary <dipeshch040313@gmail.com>
2024-09-13 15:32:43 +0530

    app.tsx changes synced

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { LandingPage } from './page/LandingPg'; // Assuming LandingPg is also in src/page
-import StudentServices from './page/StudentServices';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/student-services" element={<StudentServices />} />
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
-  );
-}
-=======
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LandingPage } from "./page/LandingPg";
@@ -28,6 +12,5 @@ const App: React.FC = () => {
     </Router>
   );
 };
->>>>>>> fdfb567e21d0233cf993bc3508bfb0e2c06af3f8
 
 export default App;
